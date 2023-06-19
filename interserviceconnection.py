65633fc4b2a771f1fdb63a1df1eec2fdc8f37e24
sgoli <sgoli@osius.com>
2023-06-19 13:54:22 +0530

    initial commit

---
+++ interserviceconnection.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class ServiceConnection:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def get(self, endpoint):
+        url = self.base_url + endpoint
+        response = requests.get(url)
+        return response.json()
+
+    def post(self, endpoint, data):
+        url = self.base_url + endpoint
+        response = requests.post(url, json=data)
+        return response.json()
+
+    def put(self, endpoint, data):
+        url = self.base_url + endpoint
+        response = requests.put(url, json=data)
+        return response.json()
+
+    def delete(self, endpoint):
+        url = self.base_url + endpoint
+        response = requests.delete(url)
+        return response.json()
